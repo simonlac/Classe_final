4f3aaddde0defb1b0cb57cc35ba657b88da14883
info1 <2129107@cegepoutaouais.qc.ca>
2022-05-06 13:34:57 -0400

    commit abb final

--- Abbonement.py
+++ Abbonement.py
@@ -16,7 +16,7 @@ class Abbonement:
     #####  MÉTHODE CONSTRUCTEUR  #####
     ###################################
 
-    def __init__(self,p_code_abbonnement = "",p_type_abbonnement = "",p_prix= 0.0,p_duree = ""):
+    def __init__(self,p_code_abbonnement = "",p_type_abbonnement = "",p_prix= 0.0,p_duree = "",p_date = "",):
         """
              méthode constructeur avec paramètre de défaut
              définie les différents attributs des documents
@@ -25,6 +25,7 @@ class Abbonement:
         self.type_abbonnement = p_type_abbonnement
         self.__prix = p_prix
         self.duree = p_duree
+        self.date = p_date
 
         ##################################################
         ####   Propriétés, accesseurs et mutateurs    ####
@@ -48,4 +49,60 @@ class Abbonement:
             self.__code_abbonnement = p_code_abbonnement
 
     code_abbonnement = property(_get_code_abbonnement,_set_code_abbonnement)
+
+    # propriété prix
+    def _get_prix(self):
+        """
+        Accès à l'attribut privée du prix
+        """
+
+        return self.__prix
+
+    def _set_prix(self, p_prix):
+        """
+        mutateur de __prix
+        """
+        if p_prix >= 0 or p_prix <= 60:
+            self.__prix = p_prix
+
+    prix = property(_get_prix, _set_prix)
+
+
+    def Calculer_total(self,prix,p_duree,p_type_abbonnement):
+        """
+        sert à calculer le total pour le prix
+        return le total de l'abbonement
+        """
+        total = 0
+        if p_type_abbonnement == "standard":
+            prix = 10.00
+        elif p_type_abbonnement == "origin":
+            prix = 20.00
+        elif p_type_abbonnement == "deluxe":
+            prix = 30.00
+        total = prix
+        if p_duree == "6 mois":
+            total /2
+        elif p_duree == "1 ans":
+            total*1
+        elif p_duree == "2 ans":
+            total*2
+        return total
+
+    def __str__(self):
+        """
+                Méthode spéciale d'affichage. À utiliser avec print(objet)
+                :return: Chaine à afficher
+        """
+        chaine_abbonnement = " " * 60 + "\n" + "*" * 60 + "\n\n" + "   Le code de l'abbonnement : " + self.code_abbonnement + "\n" + \
+                      "   Le type d'abbonement : " + self.type_abbonnement + "\n" + \
+                      "   Le prix : " + str(self.prix) + "\n" + \
+                      "   La durée de l'abonnement: " + self.duree + "\n" + \
+                      "   Le date du début de l'abbonement: " + self.date + "\n" + \
+                      "\n" + "*" * 60
+        return chaine_abbonnement
+
+
+
+
 
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 from Film import *
 from Jeux import *
 from Livre import *
+from Abbonement import *
 film1 = Film("aaaaa","Romeo et juliet",35,"romance","tom holland","tom cruise")
 print(film1)
 jeux1 = Jeux("aaaaaaaaaaaaaab","pokemon",45,"steve jobs",2,"carte")
 print(jeux1)
 livre1 = Livre("abcd","Shrek",50,"donkey",2002,"Harry.inc")
 print(livre1)
+abbonement1 = Abbonement("sdafhk21","Deluxe",30.00,"3 semaine","29 mars")
+print(abbonement1)
